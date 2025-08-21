94fd04acf4c676425721ff675db01d63ded2d6e3
bntrtm <git.bntrtm@tjbainter.com>
2025-08-21 14:43:20 -0500

    chore: database setup

--- main.go
+++ main.go
@@ -1,17 +1,63 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
+
 	"log"
+	"os"
+	"fmt"
+	"database/sql"
 	"sync/atomic"
 	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/bntrtm/chirpy/internal/database"
 )
+
+type apiConfig struct {
+	// atomic.Int32 is a //standard-library type that allows us to 
+	// safely increment and read an integer value across multiple 
+	// goroutines (HTTP requests)
+	fileserverHits	atomic.Int32
+	database		*database.Queries
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsReset(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	config := apiConfig{}
+	dbQueries := database.New(db)
+	config.database = dbQueries
 
 	mux := http.NewServeMux()
 	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
